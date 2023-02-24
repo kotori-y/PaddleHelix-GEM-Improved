8bc2d8940b2ff9cb0f7ebeafdd4108808b381280
Kotori Y <yzjkid9@gmail.com>
2023-02-24 17:53:58 +0800

    updated

--- apps/pretrained_compound/ChemRL/GEM/tasks/conf_gen/datasets/utils.py
+++ apps/pretrained_compound/ChemRL/GEM/tasks/conf_gen/datasets/utils.py
@@ -9,8 +9,6 @@ from pahelix.utils import load_json_config
 from pahelix.utils.compound_tools import mol_to_geognn_graph_data_raw3d, mol_to_geognn_graph_data_MMFF3d, \
     mol_to_geognn_graph_data
 
-from collections import namedtuple
-
 
 def load_pickled_mol_to_dataset(data_path: str):
     with open(data_path, 'rb') as f:
@@ -31,20 +29,18 @@ class ConfGenTaskTransformFn:
         self.is_inference = is_inference
 
     def __call__(self, mol):
-        _gt_pos = mol.GetConformer().GetPositions()
+        gt_pos = mol.GetConformer().GetPositions()
         # noise = np.random.uniform(-1, 1, size=_gt_pos.shape)
-        noise = 0
-        gt_pos = _gt_pos + noise
 
         prior_pos = np.random.uniform(-1, 1, gt_pos.shape)
         # prior_pos = gt_pos
 
         if not self.is_inference:
-            data = mol_to_geognn_graph_data(mol, gt_pos, dir_type='HT', only_atom_bond=True)
+            data = mol_to_geognn_graph_data(mol, gt_pos - gt_pos.mean(axis=0), dir_type='HT')
         else:
             data = None
 
-        prior_data = mol_to_geognn_graph_data(mol, prior_pos, dir_type='HT', only_atom_bond=True)
+        prior_data = mol_to_geognn_graph_data(mol, prior_pos, dir_type='HT')
 
         return [prior_data, data, prior_pos, gt_pos, mol]
 
@@ -61,7 +57,12 @@ class ConfGenTaskCollateFn(object):
 
     def __call__(self, data_list):
         prior_atom_bond_graph_list = []
+        prior_bond_angle_graph_list = []
+        prior_angle_dihedral_graph_list = []
+
         atom_bond_graph_list = []
+        bond_angle_graph_list = []
+        angle_dihedral_graph_list = []
 
         batch_list = []
         num_nodes = []
@@ -76,14 +77,44 @@ class ConfGenTaskCollateFn(object):
                 edges=prior_data['edges'],
                 node_feat={name: prior_data[name].reshape([-1, 1]) for name in self.atom_names},
                 edge_feat={name: prior_data[name].reshape([-1, 1]) for name in self.bond_names + self.bond_float_names})
+            prior_ba_g = pgl.Graph(
+                num_nodes=len(prior_data['edges']),
+                edges=prior_data['BondAngleGraph_edges'],
+                node_feat={},
+                edge_feat={name: prior_data[name].reshape([-1, 1]) for name in
+                           compound_encoder_config["bond_angle_float_names"]})
+            prior_adi_g = pgl.graph.Graph(
+                num_nodes=len(prior_data['BondAngleGraph_edges']),
+                edges=prior_data['AngleDihedralGraph_edges'],
+                node_feat={},
+                edge_feat={name: prior_data[name].reshape([-1, 1]) for name in
+                           compound_encoder_config["dihedral_angle_float_names"]})
+            prior_atom_bond_graph_list.append(prior_ab_g)
+            prior_bond_angle_graph_list.append(prior_ba_g)
+            prior_angle_dihedral_graph_list.append(prior_adi_g)
 
-            ab_g = None
             if data is not None:
                 ab_g = pgl.Graph(
                     num_nodes=len(data[self.atom_names[0]]),
                     edges=data['edges'],
                     node_feat={name: data[name].reshape([-1, 1]) for name in self.atom_names},
                     edge_feat={name: data[name].reshape([-1, 1]) for name in self.bond_names + self.bond_float_names})
+                ba_g = pgl.Graph(
+                    num_nodes=len(data['edges']),
+                    edges=data['BondAngleGraph_edges'],
+                    node_feat={},
+                    edge_feat={name: data[name].reshape([-1, 1]) for name in
+                               compound_encoder_config["bond_angle_float_names"]})
+                adi_g = pgl.graph.Graph(
+                    num_nodes=len(data['BondAngleGraph_edges']),
+                    edges=data['AngleDihedralGraph_edges'],
+                    node_feat={},
+                    edge_feat={name: data[name].reshape([-1, 1]) for name in
+                               compound_encoder_config["dihedral_angle_float_names"]})
+
+                atom_bond_graph_list.append(ab_g)
+                bond_angle_graph_list.append(ba_g)
+                angle_dihedral_graph_list.append(adi_g)
 
             batch_list.extend([i] * prior_ab_g.num_nodes)
             mol_list.append(mol)
@@ -92,30 +123,46 @@ class ConfGenTaskCollateFn(object):
             prior_pos_list.append(prior_pos)
             gt_pos_list.append(gt_pos)
 
-            prior_atom_bond_graph_list.append(prior_ab_g)
-            atom_bond_graph_list.append(ab_g)
-
         batch_list = np.array(batch_list)
         num_nodes = np.array(num_nodes)
         batch = dict(zip(["batch", "num_nodes", "mols"], [batch_list, num_nodes, mol_list]))
 
         prior_atom_bond_graph = pgl.Graph.batch(prior_atom_bond_graph_list)
-        if atom_bond_graph_list[0] is not None:
+        prior_bond_angle_graph = pgl.Graph.batch(prior_bond_angle_graph_list)
+        prior_angle_dihedral_graph = pgl.Graph.batch(prior_angle_dihedral_graph_list)
+
+        if len(atom_bond_graph_list) > 0:
             atom_bond_graph = pgl.Graph.batch(atom_bond_graph_list)
+            bond_angle_graph = pgl.Graph.batch(bond_angle_graph_list)
+            angle_dihedral_graph = pgl.Graph.batch(angle_dihedral_graph_list)
         else:
             atom_bond_graph = None
+            bond_angle_graph = None
+            angle_dihedral_graph = None
 
         # TODO: reshape due to pgl limitations on the shape
         self._flat_shapes(prior_atom_bond_graph.node_feat)
         self._flat_shapes(prior_atom_bond_graph.edge_feat)
+        self._flat_shapes(prior_bond_angle_graph.node_feat)
+        self._flat_shapes(prior_bond_angle_graph.edge_feat)
+        self._flat_shapes(prior_angle_dihedral_graph.node_feat)
+        self._flat_shapes(prior_angle_dihedral_graph.edge_feat)
 
         if atom_bond_graph is not None:
             self._flat_shapes(atom_bond_graph.node_feat)
             self._flat_shapes(atom_bond_graph.edge_feat)
+            self._flat_shapes(bond_angle_graph.node_feat)
+            self._flat_shapes(bond_angle_graph.edge_feat)
+            self._flat_shapes(angle_dihedral_graph.node_feat)
+            self._flat_shapes(angle_dihedral_graph.edge_feat)
 
         return {
             "prior_atom_bond_graphs": prior_atom_bond_graph,
-            "atom_bond_graphs": atom_bond_graph
+            "prior_bond_angle_graph": prior_bond_angle_graph,
+            "prior_angle_dihedral_graph": prior_angle_dihedral_graph,
+            "atom_bond_graphs": atom_bond_graph,
+            "bond_angle_graph": bond_angle_graph,
+            "angle_dihedral_graph": angle_dihedral_graph,
         }, batch, prior_pos_list, gt_pos_list
 
 
